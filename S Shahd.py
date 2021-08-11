5de59c91d94ca80adcf07382624d8c300fb3b11f
Shahd-ballouz <88750348+Shahd-ballouz@users.noreply.github.com>
2021-08-11 08:25:32 -0700

    create S Shahd . by

---
+++ S Shahd.py
@@ -0,0 +1,20 @@
+for row in range(3):
+    for col in range(15):
+        print("* ", end="")
+    print()
+for row in range(3):
+    for col in range(3):
+        print("* ", end="")
+    print()
+for row in range(3):
+    for col in range(15):
+        print("* ", end="")
+    print()
+for row in range(3):
+    for col in range(1):
+        print("                       ","* * * ", end="")
+    print()
+for row in range(3):
+    for col in range(15):
+        print("* ", end="")
+    print()
